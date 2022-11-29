9068cd6689f53655959566bf753415a3d8ef6c39
Marta Santos <mdpesa@gmail.com>
2022-11-29 15:02:30 +0100

    add code to 'processOrder' function

--- src/js/script.js
+++ src/js/script.js
@@ -114,7 +114,7 @@
       //   );
 
       /* START: add event listener to clickable trigger on event click */
-      thisProduct.accordionTriggerr.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
@@ -156,6 +156,50 @@
     processOrder() {
       const thisProduct = this;
       console.log('processOrder:', thisProduct);
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // for every option in this category
+          for (let optionId in param.options) {
+            // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+            const option = param.options[optionId];
+            console.log(optionId, option);
+
+            // check if there is param with a name of paramId in formData and if it includes optionId
+            if (formData[paramId] && formData[paramId].includes(optionId)) {
+              // check if the option is not default
+              if (!option.default == true) {
+                // add option price to price variable
+                price += option.price;
+              }
+            } else {
+              // check if the option is default
+              if (!option.default == false) {
+                // reduce price variable
+                price -= option.price;
+              }
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
